402dfa116fcdc714d9230483a48fe05fe0da12bd
mipra282 <mipra282@gmail.com>
2019-12-26 13:12:45 +0530

    testing in deployment

--- testing.py
+++
@@ -1,8 +0,0 @@
-from sklearn.externals import joblib
-import numpy as np
-
-knn_classifier = joblib.load("knn_model.pkl")
-
-predicted_class = knn_classifier.predict(np.array([[-1,-2.6,-0.5,-0.26]]))
-
-print(predicted_class[0])
