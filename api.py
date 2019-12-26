5d75e2b259957e2286dcd847bd402a2d67a0b5cb
mipra282 <mipra282@gmail.com>
2019-12-26 12:41:19 +0530

    Modified /predict api.py

--- api.py
+++ api.py
@@ -19,27 +19,21 @@ def predict():
 	import os
 
 
-	'''sepal_length = request.form['sepal_length']
+	sepal_length = request.form['sepal_length']
 	sepal_width = request.form['sepal_width']
 	petal_length = request.form['petal_length']
 	petal_width = request.form['petal_width']
 
 	
-	#arr = np.array(arr).astype(np.float16())
-	#[sepal_length,sepal_width,petal_length,petal_width]
+	
 	float_features = [x for x in request.form.values()]
 	final_features = [np.array(float_features)]
 
-	#if (type(sepal_length) == float  and  type(sepal_width) == float and  type(petal_length) == float and  type(petal_width) == float):
-	knn_classifier = joblib.load("/knn_model.pkl")
+	
+	knn_classifier = joblib.load(os.path.join(os.getcwd(),"knn_model.pkl"))
 
 	predicted_class = knn_classifier.predict(final_features)
-	print(predicted_class)'''
-	knn_classifier = joblib.load(os.path.join(os.getcwd(),"knn_model.pkl"))
-	predicted_class = ['Iris']
-
-	#else:
-		#predicted_class = ["Please enter float!"]
+	
 
 	return render_template("result.html",predicted = str(predicted_class[0]))
 
